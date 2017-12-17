22a143b9de53b241e945bbc8007f5eb1952fa3ca
Raja <raja.muthukalai@yahoo.com>
2017-12-17 16:14:22 +0530

    first commit

---
+++ src/main/java/com/billtracker/BilltrackerServicesApplication.java
@@ -0,0 +1,12 @@
+package com.billtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilltrackerServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilltrackerServicesApplication.class, args);
+	}
+}
