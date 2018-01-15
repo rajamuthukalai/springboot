1d290eed8530af138b208073883a8606d9714c2d
Raja <raja.muthukalai@yahoo.com>
2018-01-16 01:26:02 +0530

    renamed classes

--- src/main/java/com/billtracker/BillTrackerServicesApplication.java
+++ src/main/java/com/billtracker/BillTrackerServicesApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BilltrackerServicesApplication {
+public class BillTrackerServicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BilltrackerServicesApplication.class, args);
+		SpringApplication.run(BillTrackerServicesApplication.class, args);
 	}
 }
